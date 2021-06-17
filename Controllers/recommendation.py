3e88f0296bbb56c2c39c0513cb642b899199895f
Dayvson Silva <userddssilva@gmail.com>
2021-06-17 10:31:05 -0400

    feat: ajusta recommendation

--- Controllers/recommendation.py
+++ Controllers/recommendation.py
@@ -1,10 +1,15 @@
+import json
+
+from pprint import pprint
+
 from Controllers.connection import Connection
 from .person_correlation import person_correlation
 
-class Recomendation:
+class Recommendation:
     def __init__(self):
         self.data_base_connection = Connection()
         self.users_ratings = self.data_base_connection.get_json()
+        self.users_ratings = json.loads(self.users_ratings)
     
     def select_kneighbors_for(self, user, k=5):
         """
@@ -16,13 +21,10 @@ class Recomendation:
         distances = []
         for _user in self.users_ratings:
             if _user != user:
-                distance = person_correlation(self.users_ratings[_user],
-                                              self.users_ratings[user])
-                
-                distances.append(_user, distance)
-        sorted(distances, key=lambda coe_user: coe_user[1], reverse=True)
-        return distances
+                distance = person_correlation(self.users_ratings[_user], self.users_ratings[user])
+                distances.append((_user, distance))
+        return sorted(distances, key=lambda coe_user: coe_user[1], reverse=True)
 
     def calculating_project_valuetion(self):
-        """Calcule valuetion projected"""
+        """Calcule value projected"""
         pass
